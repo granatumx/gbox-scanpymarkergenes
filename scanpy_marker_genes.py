8b571821216235d85385c8aa7b5215d4dc59e671
David Garmire <david.garmire@gmail.com>
2023-02-17 12:17:47 -0500

    Updated...

--- scanpy_marker_genes.py
+++ scanpy_marker_genes.py
@@ -22,8 +22,11 @@ def main():
     group_vec = pd.Categorical([group_dict.get(x) for x in sample_ids])
     num_groups = len(group_vec.categories)
     figheight = 400 * (math.floor((num_groups - 1) / 7) + 1)
+    print(len(assay.index), flush=True)
+    print(len(sample_ids), flush=True)
+    print(len(group_dict), flush=True)
 
-    adata = sc.AnnData(np.array(assay.get('matrix')).transpose())
+    adata = sc.AnnData(np.array(assay.get('matrix')).T)
     adata.var_names = assay.get('geneIds')
     adata.obs_names = assay.get('sampleIds')
     adata.obs['groupVec'] = group_vec
